2cf8567e9423788d3c477a2d322bde5bedc8ef7f
Aravindh <31213085+Aravindh-Muthuswamy@users.noreply.github.com>
2021-10-24 01:28:55 +1030

    update title

--- predict_page.py
+++ predict_page.py
@@ -12,7 +12,8 @@ le_country = data['le_country']
 le_education = data['le_education']
 
 def show_predict_page():
-    st.title("""## Software Developer Salary Prediction from stack overflow dataset 2021""")
+    st.title("""Software Developer Salary Prediction from stack overflow dataset 2021""")
+    st.write("""#### by Aravindh Muthuswamy""")
     st.write("""#### we need some information to predict the salary""")
     countries = {
         "United States of America",
