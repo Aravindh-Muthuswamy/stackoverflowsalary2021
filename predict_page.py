b9ca0a08467d4e13c276678b91fc0c5210c1807b
Aravindh <31213085+Aravindh-Muthuswamy@users.noreply.github.com>
2021-10-25 14:41:01 +1030

    updated with more parameters

--- predict_page.py
+++ predict_page.py
@@ -10,6 +10,10 @@ data = load_model()
 regressor = data['model']
 le_country = data['le_country']
 le_education = data['le_education']
+le_employment = data['le_employment']
+le_gender = data['le_gender']
+le_opsys = data['le_opsys']
+le_orgsize = data['le_orgsize']
 
 def show_predict_page():
     st.title("""Software Developer Salary Prediction from stack overflow dataset 2021""")
@@ -42,14 +46,53 @@ def show_predict_page():
         "Post grad"
         
     }
+    employment = {
+        "Full Time", 
+        "Part Time", 
+        "Freelancer", 
+        "Prefer not to say",
+        "Retired"
+    }
+    gender = {
+        "Male", 
+        "Female", 
+        "Other", 
+        "Unspecified"
+    }
+    opsys = {
+        "Linux", 
+        "Windows", 
+        "MacOS", 
+        "Other", 
+        "BSD"
+    }
+    orgsize = {
+        "Small",
+        "Medium",
+        "Other",
+        "Huge",
+        "Extra Large",
+        "Tiny",
+        "Large",
+        "Freelancer",
+    }
     countrylvl = st.selectbox("Country", countries)
     educationlvl = st.selectbox("Education Level", education)
+    employmentlvl = st.selectbox('Employment Level', employment)
+    genderlvl = st.selectbox('Gender', gender)
+    opsyslvl = st.selectbox('Operating System', opsys)
+    orgsizelvl = st.selectbox('Organization Size', orgsize)
     experience = st.slider("Years of Experience", 0, 50, 3)
     ok = st.button("Calculate salary")
+    # st.write(X[: 2])
     if ok:
-        X = np.array([[countrylvl, educationlvl, experience]])
+        X = np.array([[countrylvl, educationlvl, experience, employmentlvl, genderlvl, orgsizelvl, opsyslvl]])
         X[:, 0] = le_country.transform(X[:, 0])
         X[:, 1] = le_education.transform(X[:, 1])
+        X[:, 3] = le_employment.transform(X[:, 3])
+        X[:, 4] = le_gender.transform(X[:, 4])
+        X[:, 5] = le_orgsize.transform(X[:, 5])
+        X[:, 6] = le_opsys.transform(X[:, 6])
         X = X.astype(float)
         salary = regressor.predict(X)
         st.subheader(f"The estimated salary is ${salary[0]:.2f}")
